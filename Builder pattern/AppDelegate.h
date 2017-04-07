1d2c5adb24f819a10f556e2882441ad39ab24ba5
刘伟东 <EX-LIUWEIDONG001@pingan.com.cn>
2017-04-07 14:22:20 +0800

    Initial Commit

---
+++ Builder pattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Builder pattern
+//
+//  Created by weidong liu on 2017/4/7.
+//  Copyright © 2017年 weidong liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
